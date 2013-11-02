374a5217289e014415e536e07dbd00333c7d51cf
SuYan <yansu0711@gmail.com>
2013-11-02 11:19:44 +0800

    change content display

--- public/js/base.js
+++ public/js/base.js
@@ -4,18 +4,21 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
-$(document).pjax('.pjaxtest', '#pjax', { fragment: "#pjax", timeout: 10000 });
+$(document).pjax('.pjaxlink', '#pjax', { fragment: "#pjax", timeout: 10000 });
 $(document).on("pjax:end", function() {
     $('#container').scrollTop(0);
     $('.row-offcanvas').removeClass('active');
-    $("#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
-        var current = $(this);
-        current.attr("id", "title" + i);
-        tag = current.prop('tagName').substr(-1);
-        $("#nav").append("<div style='margin-left:"+15*(tag-1)+"px'><a id='link" + i + "' href='#title" +i + "'>" + current.html() + "</a></div>");
-    });
-    $("pre").addClass("prettyprint linenums");
-    prettyPrint();
+    if($("#nav")){
+      $("#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
+          var current = $(this);
+          current.attr("id", "title" + i);
+          tag = current.prop('tagName').substr(-1);
+          $("#nav").append("<div style='margin-left:"+15*(tag-1)+"px'><a id='link" + i + "' href='#title" +i + "'>" + current.html() + "</a></div>");
+      }); 
+      $("pre").addClass("prettyprint linenums");
+      prettyPrint(); 
+    }
+    $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
     var disqus_shortname = 'suyan-zh';
     (function() {
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
