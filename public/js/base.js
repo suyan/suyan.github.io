46c0dae4a52d91e151dc1228c873c3b3d4c1bc07
Su Yan <suyan0830@gmail.com>
2013-12-05 11:01:23 +0800

    change UI to three column layout
    change UI to three column layout

--- public/js/base.js
+++ public/js/base.js
@@ -1,8 +1,17 @@
 // offcanvas
 $(document).ready(function() {
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
+  $("#nav_btn").toggle(
+    function () {
+      $('.aside').removeClass('visible-md visible-lg').addClass('hidden-md hidden-lg');
+      $('.aside3').removeClass('col-md-8 col-lg-8').addClass('col-md-12 col-lg-12');
+      $('.aside3-content').removeClass('col-md-12').addClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1');
+    },
+    function () {
+      $('.aside').addClass('visible-md visible-lg').removeClass('hidden-md hidden-lg')
+      $('.aside3').removeClass('col-md-12 col-lg-12').addClass('col-md-8 col-lg-8');
+      $('.aside3-content').removeClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1').addClass('col-md-12');
+    }
+  );
   $(document).pjax('.pjaxlink', '#pjax', { fragment: "#pjax", timeout: 10000 });
   $(document).on("pjax:end", function() {
       $('.aside3').scrollTop(0);
@@ -20,7 +29,7 @@ function contentEffects(){
         tag = current.prop('tagName').substr(-1);
         $("#nav").append("<div style='margin-left:"+15*(tag-1)+"px'><a id='link" + i + "' href='#title" +i + "'>" + current.html() + "</a></div>");
     }); 
-    $("pre").addClass("prettyprint linenums");
+    $("pre").addClass("prettyprint");
     prettyPrint(); 
     $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
   }
@@ -30,7 +39,6 @@ function contentEffects(){
   var identifier = $('#identifier').text();
   window.disqus_shortname = 'suyan-zh';
   window.disqus_identifier = identifier;
-
   function check(){
     if ( !ds_loaded && $('.aside3').scrollTop() + $('.aside3').height() >= top ) {
       $.ajax({
