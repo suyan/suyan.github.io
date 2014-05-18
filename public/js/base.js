0737d15f84323298c896a0e590c9df2b74a40f01
Su Yan <suyan0830@gmail.com>
2014-05-18 22:01:19 +0800

    add a post and some changes

--- public/js/base.js
+++ public/js/base.js
@@ -1,33 +1,69 @@
-// offcanvas
+/* 控制导航按钮动作 */
+function nav_click(is_show) {
+  if (is_show) {
+    /* 显示左侧aside */
+    $('.aside')
+      .addClass('visible-md visible-lg')
+      .removeClass('hidden-md hidden-lg')
+    /* 调整右侧内容 */
+    $('.aside3')
+      .removeClass('col-md-12 col-lg-12')
+      .addClass('col-md-8 col-lg-8');
+    /* 调整文字内容格式 */
+    $('.aside3-content')
+      .removeClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1')
+      .addClass('col-md-12');
+    /* 变化按钮 */
+    $('#nav_btn i')
+      .removeClass('fa-angle-right')
+      .addClass('fa-angle-left');
+  } else {
+    /* 隐藏左侧aside */
+    $('.aside')
+      .removeClass('visible-md visible-lg')
+      .addClass('hidden-md hidden-lg');
+    /* 右侧内容最大化 */
+    $('.aside3')
+      .removeClass('col-md-8 col-lg-8')
+      .addClass('col-md-12 col-lg-12');
+    /* 修改文字排版 */
+    $('.aside3-content')
+      .removeClass('col-md-12')
+      .addClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1');
+    /* 变化按钮图标 */
+    $('#nav_btn i').removeClass('fa-angle-left').addClass('fa-angle-right');  
+  }  
+}
+/* 控制文章章节列表按钮 */
+function content_click(is_show){
+  if (is_show) {
+    $('#content_table').show();
+    $('#content_btn i').removeClass('fa-angle-down').addClass('fa-angle-up');
+  } else {
+    $('#content_table').hide();
+    $('#content_btn i').removeClass('fa-angle-up').addClass('fa-angle-down');
+  }
+}
+
 $(document).ready(function() {
-  $("#nav_btn").on('click', function(){
-    isClicked=$(this).data('clicked');
-    if (isClicked) {isClicked=false;} else {isClicked=true;}
-    $(this).data('clicked',isClicked);
-    if(isClicked){
-      $('.aside').removeClass('visible-md visible-lg').addClass('hidden-md hidden-lg');
-      $('.aside3').removeClass('col-md-8 col-lg-8').addClass('col-md-12 col-lg-12');
-      $('.aside3-content').removeClass('col-md-12').addClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1');
-      $('#nav_btn i').removeClass('fa-angle-left').addClass('fa-angle-right');
-    }else{
-      $('.aside').addClass('visible-md visible-lg').removeClass('hidden-md hidden-lg')
-      $('.aside3').removeClass('col-md-12 col-lg-12').addClass('col-md-8 col-lg-8');
-      $('.aside3-content').removeClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1').addClass('col-md-12');
-      $('#nav_btn i').removeClass('fa-angle-right').addClass('fa-angle-left');
-    }
+  /* 控制左侧 aside 的动作 */
+  $("#nav_btn").on('click', function() {
+    isClicked = $(this).data('clicked');
+
+    nav_click(isClicked);
+
+    $(this).data('clicked', !isClicked);
   });
+
   $("#content_btn").on('click', function(){
-    isClicked=$(this).data('clicked');
-    if (isClicked) {isClicked=false;} else {isClicked=true;}
-    $(this).data('clicked',isClicked);
-    if(isClicked){
-      $('#content_table').show();
-      $('#content_btn i').removeClass('fa-angle-down').addClass('fa-angle-up');
-    }else{
-      $('#content_table').hide();
-      $('#content_btn i').removeClass('fa-angle-up').addClass('fa-angle-down');
-    }
+    isClicked = $(this).data('clicked');
+
+    content_click(!isClicked);
+
+    $(this).data('clicked',!isClicked);
+
   });
+
   $(document).pjax('.pjaxlink', '#pjax', { fragment: "#pjax", timeout: 10000 });
   $(document).on("pjax:end", function() {
     if($("body").find('.container').width() < 992)
