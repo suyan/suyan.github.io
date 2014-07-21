2c89225e30ce3c8483e6e3ff25d685a6482a892a
Su Yan <suyan0830@gmail.com>
2014-07-21 09:36:55 +0800

    let content a litter narrow on large screen

--- public/js/base.js
+++ public/js/base.js
@@ -11,7 +11,7 @@ function nav_click(is_show) {
       .addClass('col-md-8 col-lg-8');
     /* 调整文字内容格式 */
     $('.aside3-content')
-      .removeClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1')
+      .removeClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2')
       .addClass('col-md-12');
     /* 变化按钮 */
     $('#nav_btn i')
@@ -29,13 +29,13 @@ function nav_click(is_show) {
     /* 修改文字排版 */
     $('.aside3-content')
       .removeClass('col-md-12')
-      .addClass('col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1');
+      .addClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2');
     /* 变化按钮图标 */
-    $('#nav_btn i').removeClass('fa-angle-left').addClass('fa-angle-right');  
-  }  
+    $('#nav_btn i').removeClass('fa-angle-left').addClass('fa-angle-right');
+  }
 }
 /* 控制文章章节列表按钮 */
-function content_click(is_show){
+function content_click(is_show) {
   if (is_show) {
     $('#content_table').show();
     $('#content_btn i').removeClass('fa-angle-down').addClass('fa-angle-up');
@@ -45,33 +45,52 @@ function content_click(is_show){
   }
 }
 
+function content_effects() {
+  //remove the asidebar
+  $('.row-offcanvas').removeClass('active');
+  if ($("#nav").length > 0) {
+    $("#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
+      var current = $(this);
+      current.attr("id", "title" + i);
+      tag = current.prop('tagName').substr(-1);
+      $("#nav").append("<div style='margin-left:" + 15 * (tag - 1) + "px'><a id='link" + i + "' href='#title" + i + "'>" + current.html() + "</a></div>");
+    });
+    $("pre").addClass("prettyprint");
+    prettyPrint();
+    $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
+    $('#content_btn').show();
+  } else {
+    $('#content_btn').hide();
+  }
+}
+
 $(document).ready(function() {
   /* 控制左侧 aside 的动作 */
   $("#nav_btn").on('click', function() {
     isClicked = $(this).data('clicked');
-
     nav_click(isClicked);
-
     $(this).data('clicked', !isClicked);
   });
 
-  $("#content_btn").on('click', function(){
+  $("#content_btn").on('click', function() {
     isClicked = $(this).data('clicked');
-
     content_click(!isClicked);
-
-    $(this).data('clicked',!isClicked);
-
+    $(this).data('clicked', !isClicked);
   });
 
-  $(document).pjax('.pjaxlink', '#pjax', { fragment: "#pjax", timeout: 10000 });
+  $(document).pjax('.pjaxlink', '#pjax', {
+    fragment: "#pjax",
+    timeout: 10000
+  });
+
   $(document).on("pjax:end", function() {
-    if($("body").find('.container').width() < 992)
+    if ($("body").find('.container').width() < 992)
       $('#nav_btn').click();
     $('.aside3').scrollTop(0);
-    contentEffects();
+    content_effects();
   });
-  $('body').on('click', '.show-commend', function(){
+
+  $('body').on('click', '.show-commend', function() {
     var ds_loaded = false;
     window.disqus_shortname = $('.show-commend').attr('name');
     $.ajax({
@@ -81,23 +100,5 @@ $(document).ready(function() {
       cache: true
     });
   });
-  contentEffects();
+  content_effects();
 });
-function contentEffects(){
-  //remove the asidebar
-  $('.row-offcanvas').removeClass('active');
-  if($("#nav").length > 0){
-    $("#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
-        var current = $(this);
-        current.attr("id", "title" + i);
-        tag = current.prop('tagName').substr(-1);
-        $("#nav").append("<div style='margin-left:"+15*(tag-1)+"px'><a id='link" + i + "' href='#title" +i + "'>" + current.html() + "</a></div>");
-    }); 
-    $("pre").addClass("prettyprint");
-    prettyPrint(); 
-    $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
-    $('#content_btn').show();
-  }else{
-    $('#content_btn').hide();
-  }
-}
