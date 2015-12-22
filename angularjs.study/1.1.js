fd4b9fa81ba573a2fc214fb983ed5fff7717a746
chenlianghong <clh021@gmail.com>
2015-12-22 16:03:48 +0800

    update my blog

--- angularjs.study/1.1.js
+++
@@ -1,7 +0,0 @@
-angular.module('test4', []).controller('namesCtrl', function($scope) {
-    $scope.names = [
-        {name:'Jani',country:'Norway'},
-        {name:'Hege',country:'Sweden'},
-        {name:'Kai',country:'Denmark'}
-    ];
-});
--- angularjs.study/1.test4.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp', []).controller('namesCtrl', function($scope) {
-    $scope.names = [
-        {name:'Jani',country:'Norway'},
-        {name:'Hege',country:'Sweden'},
-        {name:'Kai',country:'Denmark'}
-    ];
-});
