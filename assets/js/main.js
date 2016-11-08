97b7c857bd527539dfea0bd67cca356e239f3d90
Su Yan <suyan0830@gmail.com>
2016-11-07 18:03:26 -0800

    complete table of content

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,11 @@ layout: null
 ---
 
 $(function() {
-  var toc     = $('.toc-link'),
-      sidebar = $('#sidebar'),
-      main    = $('#main'),
-      menu    = $('#menu'),
+  var toc      = $('.toc-link'),
+      sidebar  = $('#sidebar'),
+      main     = $('#main'),
+      menu     = $('#menu'),
+      righttoc = $('#right-toc-menu'),
       x1, y1;
 
   // run this function after pjax load.
@@ -15,6 +16,36 @@ $(function() {
     $('#main').find('a').filter(function() {
       return this.hostname != window.location.hostname;
     }).attr('target', '_blank');
+
+    // generate toc
+    var toc = $('#post-toc-ul');
+    // Empty TOC and generate an entry for h1
+    toc.empty().append('<li class="post-toc-li post-toc-h1"><a href="#post-title" class="js-anchor-link">' + $('#post-title').text() + '</a></li>');
+
+    // Generate entries for h2 and h3
+    $('.post').children('h2,h3').each(function() {
+      // Generate random ID for each heading
+      $(this).attr('id', function() {
+        var ID = "", alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        for(var i=0; i < 5; i++) {
+          ID += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        }
+        return ID;
+      });
+
+      if ($(this).prop("tagName") == 'H2') {
+        toc.append('<li class="post-toc-li post-toc-h2"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
+      } else {
+        toc.append('<li class="post-toc-li post-toc-h3"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
+      }
+    });
+
+    // Smooth scrolling
+    $('.js-anchor-link').on('click', function() {
+      var target = $(this.hash);
+      main.animate({scrollTop: target.offset().top + main.scrollTop() - 70}, 500);
+    });
 
     // discus comment.
     {% if site.disqus.shortname %}
@@ -41,7 +72,6 @@ $(function() {
 
   // NProgress
   NProgress.configure({ showSpinner: false });
-
 
   // Pjax
   $(document).pjax('#sidebar-avatar, .toc-link', '#main', {
@@ -82,6 +112,10 @@ $(function() {
   // Menu
   menu.on('click', function() {
     $(this).add(sidebar).add(menu).add(main).toggleClass('open');
+  });
+
+  righttoc.on('click', function() {
+    $('#post-toc').toggleClass('open');
   });
 
 });
